bdee50bfee099b33c4d29c4d8b8d419a963c5845
Edu Renesto <edure95@gmail.com>
2020-02-23 23:04:46 -0300

    Shape: Add triangle intersection

--- src/tracer/shape.rs
+++ src/tracer/shape.rs
@@ -13,6 +13,7 @@ use super::ray::{Ray, RayHit};
 pub enum Shape {
     Sphere(Material, Vec3<f32>, f32),
     Plane(Material, Vec3<f32>, Vec3<f32>),
+    Poly(Material, Triangle),
 }
 
 #[derive(Debug)]
@@ -81,6 +82,52 @@ impl Shape {
                     })
                 }
             }
+            &Shape::Poly(_, triangle) => {
+                // TODO otimizar essa caralaha.
+                // Reasoning:
+                //
+                // Primeiro checamos a interseccao raio-plano.
+
+                let v01 = triangle.vertices[1] - triangle.vertices[0];
+                let v02 = triangle.vertices[2] - triangle.vertices[0];
+                let v12 = triangle.vertices[2] - triangle.vertices[1];
+
+                let normal = v01.cross(v02).normalized();
+
+                let nr = normal.dot(ray.direction);
+                let u = ray.origin - triangle.vertices[0];
+                let t = ((u.dot(normal)) / nr);
+
+                let point = ray.origin + t * ray.direction;
+
+                if nr == 0f32 || t <= 0f32 {
+                    return None;
+                }
+
+                // Então checamos se a projeção entre o vetor que leva dos vértices
+                // ao ponto e as arestas é menor que 1.
+                // Se sim, então está dentro do triangulo
+                // Senão, está fora.
+
+                let v0p = point - triangle.vertices[0];
+                let v1p = point - triangle.vertices[1];
+                let v2p = point - triangle.vertices[2];
+
+                let w0 = v0p.dot(v01).abs() <= 1f32 && v0p.dot(v02).abs() <= 1f32;
+                let w1 = v1p.dot(v01).abs() <= 1f32 && v1p.dot(v12).abs() <= 1f32;
+                let w2 = v2p.dot(v02).abs() <= 1f32 && v2p.dot(v12).abs() <= 1f32;
+
+                if w0 && w1 && w2 {
+                    println!("Trig hit");
+                    Some(RayHit {
+                        ray: &ray,
+                        distance: t,
+                        object: *self,
+                    })
+                } else {
+                    None
+                }
+            }
         }
     }
     pub fn normal_at<'a>(&self, hit: &RayHit<'a>) -> Vec3<f32> {
@@ -90,12 +137,26 @@ impl Shape {
                 normal.normalized()
             }
             &Shape::Plane(_, _, normal) => normal,
+            &Shape::Poly(_, trig) => {
+                let v0 = trig.vertices[1] - trig.vertices[0];
+                let v1 = trig.vertices[2] - trig.vertices[0];
+
+                v0.cross(v1).normalized()
+            }
         }
     }
     pub fn material(&self) -> Material {
         match self {
             &Shape::Sphere(mat, _, _) => mat,
             &Shape::Plane(mat, _, _) => mat,
+            &Shape::Poly(mat, _) => mat,
         }
     }
+}
+
+#[derive(Copy, Clone)]
+pub struct Triangle {
+    pub vertices: [vek::Vec3<f32>; 3],
+    pub normals: Option<[vek::Vec3<f32>; 3]>,
+    pub tex_coords: Option<[vek::Vec2<f32>; 3]>,
 }
